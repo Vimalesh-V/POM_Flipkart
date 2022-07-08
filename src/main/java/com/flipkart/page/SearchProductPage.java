20b2c1d55307453e49183a3ed3acb5a054bd144b
Vimalesh-V <vimalesh187@gmail.com>
2022-07-08 18:31:30 +0530

    First Commit

---
+++ src/main/java/com/flipkart/page/SearchProductPage.java
@@ -0,0 +1,14 @@
+package com.flipkart.page;
+
+import org.openqa.selenium.By;
+
+public class SearchProductPage {
+
+	By searchLoc = By.xpath("");
+	
+	public void validateSearchProduct(String productName) {
+		
+	}
+}
+
+
